ec392ce3cae256030e8dae943c608ef59c7cdd5c
MrSaharok <zhelticov@gmail.com>
2023-11-10 23:23:14 +0700

    Rack

---
+++ run_time.rb
@@ -0,0 +1,20 @@
+require './time_service'
+
+class RunTime
+
+  def call(env)
+    response = Rack::Request.new(env)
+    time = TimeService.new(response)
+    if time.errors.empty?
+      answer(time.parse, 200)
+    else
+      answer(time.errors, 400)
+    end
+  end
+
+  private
+
+  def answer(body, status)
+    Rack::Response.new(body, status).finish
+  end
+end
